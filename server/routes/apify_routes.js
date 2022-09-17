53e02505622e0af907bc994792fdd09fb733f3c0
SuyogJo <suyoghero@gmail.com>
2022-09-17 16:35:14 -0400

    scraping an instagram profile for username and comments using apify API

---
+++ server/routes/apify_routes.js
@@ -0,0 +1,36 @@
+const { ApifyClient } = require('apify-client');
+const express = require("express");
+const router = express.Router();
+
+// Initialize the ApifyClient with API token
+const client = new ApifyClient({
+    token: 'apify_api_CO6Gmc3rRT2mtH9ggWI35wQ4QJ1lYO1u50DD',
+});
+
+// Prepare actor input
+const input = {
+    "directUrls": [
+        "hhttps://www.instagram.com/p/CKSwhnBJK-i/",
+        "https://www.instagram.com/p/CGdl6M_Jh1l/"
+    ],
+    "resultsLimit": 5
+};
+
+router.route("/").get((req, res) => {
+    (async () => {
+        // Run the actor and wait for it to finish
+
+        const run = await client.actor("zuzka/instagram-comment-scraper").call(input);
+
+        // Fetch and print actor results from the run's dataset (if any)
+        console.log('Results from dataset');
+        const { items } = await client.dataset(run.defaultDatasetId).listItems();
+        console.log(items)
+        return res.status(200).send(JSON.stringify(items));
+        items.forEach((item) => {
+            console.log(item);
+        });
+    })();
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
-const port = 3000;
+
+const bp = require("body-parser");
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+const apiRoutes = require("./routes/apify_routes");
+
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
