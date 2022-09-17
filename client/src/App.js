f046f2ac5fe9ef52defe2bd918a6bd1d3c7e15e9
Nicole Cevey <nicole.cevey@gmail.com>
2022-09-17 18:48:30 -0400

    Styling landing and form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/global.scss";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import StartPage from "./pages/StartPage/StartPage";
 import LoadingPage from "./pages/LoadingPage/LoadingPage";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import { Navigation } from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/start" element={<StartPage />} />
