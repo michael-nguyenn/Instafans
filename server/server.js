cb5c457d1b4834f5c15e3064d97bfeb359319734
Michael Nguyen <n93michael@gmail.com>
2022-09-17 13:27:07 -0400

    added backend routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT ?? 8080;
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
