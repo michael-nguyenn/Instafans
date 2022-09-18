a8d4e086c190caeb98c94b3ddd14aaf2c9f2ee9d
Nicole Cevey <nicole.cevey@gmail.com>
2022-09-18 01:45:47 -0400

    Adding dashboard feature

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,13 +1,20 @@
 import "./Navigation.scss";
 import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export function Navigation() {
+  let { userId } = useParams();
+  console.log(userId);
   return (
     <nav className="navbar">
       <Link to="/" className="navbar__link">
         <h3 className="navbar__text">InstaFans</h3>
         <img src={logo} className="navbar__logo"></img>
+      </Link>
+      {/* {if (useId = dashboard) } */}
+      <Link to="/start" className="navbar__account">
+        Try another account
       </Link>
     </nav>
   );
--- client/src/components/StartForm/StartForm.js
+++ client/src/components/StartForm/StartForm.js
@@ -1,34 +1,75 @@
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import "./StartForm.scss";
-// import Box from "@mui/material/Box";
-// import Slider from "@mui/material/Slider";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+
+function valuetext(value) {
+  return `${value}`;
+}
 
 export default function StartForm() {
+  const [value, setValue] = useState([35]);
   const navigate = useNavigate();
-  const handleClick = () => {
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleSubmit = () => {
     navigate("/loading");
   };
+
+  const marks = [
+    {
+      value: 1,
+      label: "1",
+    },
+    {
+      value: 100,
+      label: "24",
+    },
+  ];
+
+  // const theme = createTheme({
+  //   status: {
+  //     danger: "#e53e3e",
+  //   },
+  //   palette: {
+  //     primary: {
+  //       main: "#2D97B9",
+  //       darker: "#053e85",
+  //     },
+  //     neutral: {
+  //       main: "#64748B",
+  //       contrastText: "#fff",
+  //     },
+  //   },
+  // });
+
   return (
     <>
       <form className="form">
         <div className="form__container">
           <h2 className="form__header">
-            We just need a few things to get started
+            We just need a few things to get started...
           </h2>
           <p>What's your instagram handle?</p>
           <input placeholder="@Lydiash.an"></input>
-          <p>How many comments do you want us to analyze?</p>
-          {/* <Box>
+          <p className="form__text">
+            How many comments do you want us to analyze?
+          </p>
+          <Box sx={{ width: 400 }} className="form__slider">
             <Slider
-              getAriaLabel={() => "Minimum distance"}
-              value={value1}
-              onChange={handleChange1}
-              valueLabelDisplay="auto"
+              getAriaLabel={() => "Comment range"}
+              value={value}
+              onChange={handleChange}
+              valueLabelDisplay="off"
               getAriaValueText={valuetext}
-              disableSwap
+              marks={marks}
             />
-          </Box> */}
-          <button className="form__button" onClick={handleClick()}>
+          </Box>
+          <button className="form__button" onClick={handleSubmit}>
             Continue
           </button>
         </div>
